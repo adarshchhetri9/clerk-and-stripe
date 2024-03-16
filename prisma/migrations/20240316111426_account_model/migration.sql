6ba91a7351d85df97fe386a6f872eef4e66aa078
Adarsh chhetri <chhetriadarsh912@gmail.com>
2024-03-16 04:36:55 -0700

    need to complete clerk setup

---
+++ prisma/migrations/20240316111426_account_model/migration.sql
@@ -0,0 +1,24 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Post` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+PRAGMA foreign_keys=off;
+DROP TABLE "Post";
+PRAGMA foreign_keys=on;
+
+-- CreateTable
+CREATE TABLE "Account" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "userId" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "stripeCustomerId" TEXT,
+    "package" TEXT,
+    "status" TEXT NOT NULL DEFAULT 'INACTIVE'
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Account_userId_key" ON "Account"("userId");
---
+++ src/pages/api/webhooks/clerk.ts
@@ -0,0 +1,4 @@
+import { Webhook } from "svix";
+import { buffer } from "micro";
+import { env } from "@/env";
+import { db } from "@/server/db";
